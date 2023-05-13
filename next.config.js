b375ef9195ff8007719312ab6310a613e7a54f47
Samihan <ssmuppirala@gmail.com>
2023-05-12 22:51:56 -0700

    Updating API url to work
    Decided to use serveo because it's free ! (and also I couldn't get the CNAME redirection on samihansclub.com to work)

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   publicRuntimeConfig: {
     // https://stackoverflow.com/a/75208523
     // look at https://github.com/node-fetch/node-fetch/issues/1624#issuecomment-1407717012 if it still doesn't work
-    apiUrl: "http://api.samihansclub.com",
+    apiUrl: "http://rudra-riot-stats.serveo.net", // "http://api.samihansclub.com",
   },
   images: {
     remotePatterns: [
